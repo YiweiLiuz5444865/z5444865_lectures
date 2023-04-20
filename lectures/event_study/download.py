c836ba475b6c9e87ba55a298545d9558e6cbeb50
Yiwei Liu <129968742+YiweiLiuz5444865@users.noreply.github.com>
2023-04-20 17:13:00 +1000

    Add files via upload

--- lectures/event_study/download.py
+++ lectures/event_study/download.py
@@ -5,37 +5,18 @@ Utilities to download data from Yahoo Finance
 
 import yfinance as yf
 
-#from event_study import config as cfg
-#import config as cfg
-
+from event_study import config as cfg
 
 # -------------------------------------------------------- 
 #   Function to download recommendations
 # --------------------------------------------------------
-def yf_rec_to_csv(tic, pth,
-                  start=None,
-                  end=None):
+def yf_rec_to_csv(tic, pth, start=None, end=None):
     """ Downloads analysts recommendation from Yahoo Finance and saves the
-    information in a CSV file
+    information in a CSV file"""
 
-    Parameters
-    ----------
-    tic : str
-        Ticker
-
-    pth : str
-        Location of the output CSV file
-
-    start: str, optional
-        Download start date string (YYYY-MM-DD)
-        If None (the default), start is set to '1900-01-01'
-
-    end: str, optional
-        Download end date string (YYYY-MM-DD)
-        If None (the default), end is set to the most current date available
-    """
     c = yf.Ticker(tic)
     c.history(start=start, end=end)
+
     # Make sure we only relevant dates
     if start is not None and end is not None:
         df = c.recommendations.loc[start:end]
@@ -72,12 +53,12 @@ def get_data(tic):
     print('Done')
 
     # Download and save recs
-    print(f'Downloading recs for {tic}...')
-    yf_rec_to_csv(tic, 
-            pth=locs['rec_csv'], 
-            start=cfg.START,
-            end=cfg.END)
-    print('Done')
+    #print(f'Downloading recs for {tic}...')
+    #yf_rec_to_csv(tic,
+    #        pth=locs['rec_csv'],
+    #        start=cfg.START,
+    #        end=cfg.END)
+    #print('Done')
 
 
 if __name__ == "__main__":
